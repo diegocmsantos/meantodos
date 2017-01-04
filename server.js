c1e82e3895c230798850fa1c5b70db5a1ecee25c
Diego Maia <diegocmsantos@gmail.com>
2017-01-03 23:37:30 -0300

    Structuring project, create index and todos routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const todos = require('./routes/todos');
+
+const app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/api/v1/', todos);
+
+app.listen(3000, function() {
+  console.log('server started on port 3000');
+});
